9912e7e3ef648a0eefe3e682f7818e879c3298b9
Maxim Penzin <maxp@ya.ru>
2018-01-28 18:53:42 +0800

    run-loop update

--- env.sh
+++ env.sh
@@ -1,5 +1,5 @@
 
 export PROJECT_NAME="myapp"
 export PROD_HOST="app@__HOST__"
-export PROD_PATH="/app"${PROJECT_NAME}
-export PROD_LOG="__DIR__/"${PROJECT_NAME}.log
+export PROD_PATH="/app/"${PROJECT_NAME}
+export PROD_LOG="./__DIR__/"${PROJECT_NAME}.log
